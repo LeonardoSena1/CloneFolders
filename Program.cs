71a5635978c26eb393d518a2e0425a5389c1c06f
Leonardo Sena <leonardo_sena1@hotmail.com>
2023-08-14 17:11:28 -0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string sourcePath = @"E:";
+        string targetPath = @"C:\Users\leona\Downloads\teste";
+
+        try
+        {
+            CopyFolder(sourcePath, targetPath);
+        }
+        catch (UnauthorizedAccessException ex)
+        {
+            throw new Exception($"Access denied: {ex.Message}");
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+
+        Console.WriteLine("Disk cloning complete.");
+    }
+
+    static void CopyFolder(string sourcePath, string targetPath)
+    {
+        if (!Directory.Exists(targetPath))
+            Directory.CreateDirectory(targetPath);
+
+        string[] files = Directory.GetFiles(sourcePath);
+        foreach (string file in files)
+            File.Copy(
+                file,
+                Path.Combine(targetPath, Path.GetFileName(file)).ToString(),
+                true);
+
+        string[] subDirectories = Directory.GetDirectories(sourcePath);
+        foreach (string subDir in subDirectories)
+            if (!subDir.Contains("System Volume Information"))
+                CopyFolder(subDir, Path.Combine(targetPath, Path.GetFileName(subDir)));
+    }
+}
